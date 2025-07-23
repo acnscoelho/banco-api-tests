062b09d9305d874f86a222c2e94f855bf0980551
acnscoelho <acnscoelho@hotmail.com>
2025-07-23 18:58:04 -0300

    Adicionando a biblioteca .env para variaveis globais

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,11 +1,12 @@
 const request = require('supertest');
 const { expect } = require('chai')
+require('dotenv').config()
 
 describe('Transferencias', () => {
     describe('POST /transferencias', ()=> {
         it('Deve retornar sucesso com 201 quando o valor da tranferência for igual ou acima de R$ 10,00', async () => {
             // Capturar o token
-            const respostaLogin = await request('http://localhost:3000')
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
@@ -15,7 +16,7 @@ describe('Transferencias', () => {
 
             const token = respostaLogin.body.token    
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
             .post('/transferencias')
             .set('Content-Type', 'application/json')
             .set('Authorization', `Bearer ${token}`)
@@ -30,7 +31,7 @@ describe('Transferencias', () => {
         })
 
         it('Deve retornar falha com 422 quando o valor da transferência foi igual ou acima de R$ 10,00', async () => {
-            const respostaLogin = await request('http://localhost:3000')
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
@@ -40,7 +41,7 @@ describe('Transferencias', () => {
 
             const token = respostaLogin.body.token    
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
             .post('/transferencias')
             .set('Content-Type', 'application/json')
             .set('Authorization', `Bearer ${token}`)
